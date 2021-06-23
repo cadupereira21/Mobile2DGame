f49ac489c68175e9f088ad310fb67bb862e8b878
Carlos <carloseduardo2101@gmail.com>
2021-06-23 17:38:08 -0300

    code update - inheritance implementation at Animation classes

--- Assets/Game/Scripts/Player/CharacterAnimationController.cs
+++ Assets/Game/Scripts/Player/CharacterAnimationController.cs
@@ -19,54 +19,17 @@ public static class EnemyAnimationKeys
 
 public class CharacterAnimationController : MonoBehaviour
 {
-    Animator animator;
-    CharacterMovement2D playerMovement;
+    protected Animator animator;
+    protected CharacterMovement2D playerMovement;
 
-    EnemyAIController aiController;
-    PlayerController playerController;
-    IDamageable damageable;
-
-    private void Awake()
+    protected virtual void Awake()
     {
         animator = GetComponent<Animator>();
         playerMovement = GetComponent<CharacterMovement2D>();
-        aiController = GetComponent<EnemyAIController>();
-        playerController = GetComponent<PlayerController>();
-
-        damageable = GetComponent<IDamageable>();
-        if (damageable != null)
-        {
-            damageable.DeathEvent += OnDeath;
-        }
     }
 
-    private void Update()
+    protected virtual void Update()
     {
         animator.SetFloat(CharacterMovementAnimationKeys.HorizontalSpeed, playerMovement.CurrentVelocity.x / playerMovement.MaxGroundSpeed);
-
-        if (playerController != null)
-        {
-            animator.SetBool(CharacterMovementAnimationKeys.IsCrouching, playerMovement.IsCrouching);
-            animator.SetFloat(CharacterMovementAnimationKeys.VerticalSpeed, playerMovement.CurrentVelocity.y / playerMovement.JumpSpeed);
-            animator.SetBool(CharacterMovementAnimationKeys.IsGrounded, playerMovement.IsGrounded);
-        }
-
-        if (aiController != null)
-        {
-            animator.SetBool(EnemyAnimationKeys.IsChasing, aiController.IsChasing);
-        }
-    }
-
-    private void OnDestroy()
-    {
-        if (damageable != null)
-        {
-            damageable.DeathEvent -= OnDeath;
-        }
-    }
-
-    private void OnDeath()
-    {
-        animator.SetTrigger(CharacterMovementAnimationKeys.Dead);
     }
 }
---
+++ Assets/Game/Scripts/Player/PlayerAnimationController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : CharacterAnimationController
+{
+    IDamageable damageable;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        damageable = GetComponent<IDamageable>();
+        if (damageable != null)
+        {
+            damageable.DeathEvent += OnDeath;
+        }
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        animator.SetBool(CharacterMovementAnimationKeys.IsCrouching, playerMovement.IsCrouching);
+        animator.SetFloat(CharacterMovementAnimationKeys.VerticalSpeed, playerMovement.CurrentVelocity.y / playerMovement.JumpSpeed);
+        animator.SetBool(CharacterMovementAnimationKeys.IsGrounded, playerMovement.IsGrounded);
+    }
+
+    private void OnDestroy()
+    {
+        if (damageable != null)
+        {
+            damageable.DeathEvent -= OnDeath;
+        }
+    }
+
+    private void OnDeath()
+    {
+        animator.SetTrigger(CharacterMovementAnimationKeys.Dead);
+    }
+}
